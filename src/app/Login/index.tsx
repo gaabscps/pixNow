ac9bf7aee5a69aed69bb679e2d2a1ef003edee8c
Gabriel Andrade <gaabscps@gmail.com>
2024-11-26 00:46:39 -0300

    🎉 Project start Removed unused files and setup chakraUI

---
+++ src/app/Login/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@/components/Text";
+import { Box, Flex } from "@chakra-ui/react";
+import { LoginForm } from "./components/Form";
+
+export const Login: React.FC = () => {
+  return (
+    <Box
+    >
+      Hello World
+    </Box>
+  );
+};
